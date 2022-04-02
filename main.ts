905bc06977e0a04eac19f0ee4622b9c2a0d89540
sololim <101944276+sololim@users.noreply.github.com>
2022-04-02 12:44:45 -0400

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,14 @@
+input.onGesture(Gesture.Shake, function () {
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
+    basic.showIcon(IconNames.Diamond)
+    basic.showIcon(IconNames.SmallDiamond)
+    if (Math.randomBoolean()) {
+        basic.showIcon(IconNames.Skull)
+    } else {
+        basic.showIcon(IconNames.Square)
+    }
+})
+basic.forever(function () {
+	
+})
